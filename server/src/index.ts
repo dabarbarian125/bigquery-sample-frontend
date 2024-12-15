35ce4aa13d615a4c5ee1f3c4acad1e254b42201a
np <noahpierce125@gmail.com>
2024-12-15 14:14:01 -0500

    changed routing in server2

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,12 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.use('/api', router);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 const port = 8080;
